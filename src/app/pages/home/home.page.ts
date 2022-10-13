605c5a61c0c8e757035d3bf6b65a7234a8da9bc0
fate-9x <jor.nancupil@duocuc.cl>
2022-10-13 12:50:49 -0300

    Scanner listo

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,23 +1,28 @@
 /** Importaciones de librerias a usar */
 
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AnimationController, ToastController } from '@ionic/angular';
-
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 // Decorador Componente este indica que el Home Page es un Componente
 @Component({
   selector: 'app-home', // Nombre del selector como <input> o <main-page>
   templateUrl: 'home.page.html', // Arhivo HTML de la visual a trabajar
   styleUrls: ['home.page.scss'], // Archivo/s de estilos
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
 
-  @ViewChild('animacion1', {read:ElementRef, static:true}) animacion1:ElementRef;
+  @ViewChild('animacion1', { read: ElementRef, static: true }) animacion1: ElementRef;
 
-  user:any;
-  data: any={
-    seccion:"",
-  }; // Generamos una variable Any (permite cualquier valor)
+  user: any;
+  data: any = {
+    seccion: "",
+  };
+  // https://www.npmjs.com/package/angularx-qrcode
+  qrCodeString = 'This is a secret qr code message';
+  scannedResult: any;
+  content_visibility = '';
+  // Generamos una variable Any (permite cualquier valor)
 
   // niveles:any[]=[
   //   {id:1,nivel:"Basica Incompleta"},
@@ -36,19 +41,79 @@ export class HomePage {
    * : Indica que el identificador sera de la clase posterior a los : puntos
    * 
    */
-  constructor(private activeroute: ActivatedRoute, private router: Router, public toastController:ToastController, private animationCtrl: AnimationController) {
+  constructor(private activeroute: ActivatedRoute, private router: Router, public toastController: ToastController, private animationCtrl: AnimationController) {
+
     // Se llama a la ruta activa y se obtiene sus parametros mediante una subscripcion
     this.activeroute.queryParams.subscribe(params => { // Utilizamos lambda
       if (this.router.getCurrentNavigation().extras.state) { // Validamos que en la navegacion actual tenga extras
+        this.content_visibility = "show";
         this.user = this.router.getCurrentNavigation().extras.state.user; // Si tiene extra rescata lo enviado
-         // Muestra por consola lo traido
-      }else{this.router.navigate(["/login"])} // Si no tiene extra la navegacion actual navegar al login
+        // Muestra por consola lo traido
+      } else { this.router.navigate(["/login"]) } // Si no tiene extra la navegacion actual navegar al login
     });
   }
+
   ngOnInit() {
   }
 
 
+  // startScan() {
+  //   this.barcodeScanner.scan().then(barcodeData => {
+  //     console.log('Barcode data', barcodeData);
+  //     this.scannedResult = barcodeData?.text;
+  //    }).catch(err => {
+  //        console.log('Error', err);
+  //    });
+  // }
+
+  async checkPermission() {
+    try {
+      // check or request permission
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        // the user granted permission
+        return true;
+      }
+      return false;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async startScan() {
+    try {
+      const permission = await this.checkPermission();
+      if (!permission) {
+        return;
+      }
+      await BarcodeScanner.hideBackground();
+      document.querySelector('body').classList.add('scanner-active');
+      this.content_visibility = 'hidden';
+      const result = await BarcodeScanner.startScan();
+      console.log(result);
+      BarcodeScanner.showBackground();
+      document.querySelector('body').classList.remove('scanner-active');
+      this.content_visibility = '';
+      if (result?.hasContent) {
+        this.scannedResult = result.content;
+        console.log(this.scannedResult);
+      }
+    } catch (e) {
+      console.log(e);
+      this.stopScan();
+    }
+  }
+
+  stopScan() {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document.querySelector('body').classList.remove('scanner-active');
+    this.content_visibility = '';
+  }
+
+  ngOnDestroy(): void {
+    this.stopScan();
+  }
   // mostrar(){
   //   if(this.data.nombre!="" && this.data.apellido!=""){
   //     this.presentAlert("Usuario", "Su nombre es: "+this.data.nombre+ " " + this.data.apellido)
